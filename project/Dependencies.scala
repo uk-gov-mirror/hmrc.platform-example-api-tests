7e83b813d6a44223bf56203c098a091cf8f0ad74
ejayaraman <7340261+ejayaraman@users.noreply.github.com>
2021-02-17 13:57:05 +0000

    PLATUI-564: Update to cucumber version to 6.9.1 Included cucumber.properties to quieten publish messages.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,8 +4,8 @@ object Dependencies {
 
   val test = Seq(
     "org.scalatest"     %% "scalatest"              % "3.0.7"  % Test,
-    "io.cucumber"       %% "cucumber-scala"         % "6.1.1"  % Test,
-    "io.cucumber"        % "cucumber-junit"         % "6.1.1"  % Test,
+    "io.cucumber"       %% "cucumber-scala"         % "6.9.1"  % Test,
+    "io.cucumber"        % "cucumber-junit"         % "6.9.1"  % Test,
     "junit"              % "junit"                  % "4.12"   % Test,
     "com.novocode"       % "junit-interface"        % "0.11"   % Test,
     "com.typesafe"       % "config"                 % "1.3.2"  % Test,
